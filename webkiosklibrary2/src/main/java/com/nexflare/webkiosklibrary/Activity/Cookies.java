1ca6525bddf3f416e1c018884e3c0776a4b3c0bf
NexFlare <NexFlare@users.noreply.github.com>
2017-09-24 22:54:42 +0530

    Cookies for Thapar University Added

--- webkiosklibrary2/src/main/java/com/nexflare/webkiosklibrary/Activity/Cookies.java
+++ webkiosklibrary2/src/main/java/com/nexflare/webkiosklibrary/Activity/Cookies.java
@@ -12,7 +12,7 @@ import java.util.Map;
  * Created by Dheerain on 24-09-2017.
  */
 
-public class Connect {
+public class Cookies {
 
 
     public static Map<String, String> getCookiesForJaypee(String enrollmentNumber, String dateOfBirth, String password, String college) throws IOException {
@@ -41,21 +41,17 @@ public class Connect {
 
 
 
-    public static Map<String, String> getCookiesForThapar(String enrollmentNumber, String dateOfBirth, String password, String college) throws IOException {
+    public static Map<String, String> getCookiesForThapar(String enrollmentNumber, String password) throws IOException {
         Connection.Response login = Jsoup.connect(Constants.BASE_URL_THAPAR)
                 .method(Connection.Method.GET)
                 .userAgent(Constants.AGENT_MOZILLA)
                 .execute();
 
         Connection.Response studentPage = Jsoup.connect(Constants.BASE_URL_THAPAR_LOGIN)
-                .data("txtInst", "Institute")
-                .data("InstCode", college)
                 .data("txtuType", "Member Type")
                 .data("UserType", "S")
                 .data("txtCode", "Enrollment No")
                 .data("MemberCode", enrollmentNumber)
-                .data("DOB", "DOB")
-                .data("DATE1", dateOfBirth)
                 .data("txtPin", "Password/Pin")
                 .data("Password", password)
                 .data("BTNSubmit", "Submit")
@@ -65,7 +61,7 @@ public class Connect {
         return studentPage.cookies();
     }
 
-    public Connect() throws IOException {
+    public Cookies() throws IOException {
     }
 
 
