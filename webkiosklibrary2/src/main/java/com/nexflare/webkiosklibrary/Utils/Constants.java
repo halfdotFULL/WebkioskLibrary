1343a849fd2042a928f5b8e6984b5a8798faa897
NexFlare <NexFlare@users.noreply.github.com>
2017-09-24 20:07:26 +0530

    Constants for university added

---
+++ webkiosklibrary2/src/main/java/com/nexflare/webkiosklibrary/Utils/Constants.java
@@ -0,0 +1,10 @@
+package com.nexflare.webkiosklibrary.Utils;
+
+/**
+ * Created by nexflare on 24/09/17.
+ */
+
+public class Constants {
+    public static final String BASE_URL_THAPAR="https://webkiosk.thapar.edu/CommonFiles/UserAction.jsp";
+    public static final String BASE_URL_WEBKISOK="https://webkiosk.jiit.ac.in/CommonFiles/UserActionn.jsp";
+}
