69bdd097a7f6d3de3f32d70da467ab5d939fa91e
dheerain-jain <jain.dheerain@gmail.com>
2017-09-26 04:37:35 +0530

    CommentedTheCodeAdded

--- webkiosklibrary2/src/main/java/com/nexflare/webkiosklibrary/Apis/Jiit/Cgpa/WebkioskCgpaReport.java
+++ webkiosklibrary2/src/main/java/com/nexflare/webkiosklibrary/Apis/Jiit/Cgpa/WebkioskCgpaReport.java
@@ -27,24 +27,34 @@ public class WebkioskCgpaReport implements WebkioskContract<CgpaReportResult>{
 
         public WebkioskCgpaReport() {
             mResultHandler = new Handler(Looper.getMainLooper());
+            /*as thread normally doesn't have a looper inbuilt
+            * it finishes after the task is over
+            * to recieve the call back we need to impliment the looper for the message que to be implimented*/
         }
+            /*we first need to validate the user to gett the cgpa and spa ,
+            *so here we use cookies to get the user loged in once and then use
+            * the cookies to get hte user verified every time */
 
         public WebkioskCgpaReport getCgpaReport(final String enrollmentNumber, final String dateOfBirth, final String password,final String college, final String url){
+
+            /*running on another thread because crawling is a blocking event*/
 
             Thread thread=new Thread(new Runnable() {
                 @Override
                 public void run() {
                     try{
 
+                        /*get cookies here after login*/
                         Map<String, String> cookies = Cookies.getCookiesForJaypee(enrollmentNumber, dateOfBirth, password,college);
 
                         Document document = Jsoup.connect(url)
                                 .cookies(cookies)
                                 .userAgent(Constants.AGENT_MOZILLA)
                                 .execute().parse();
+                        /*get the html page here in the document */
 
                         final CgpaReportResult cgpaReportResult = new CgpaReportResult();
-
+                        /*check if cookies went stale fo  that the user can be validated again*/
                         if (document.body().toString().toLowerCase().contains("session timeout")) {
                             throw new InvalidCredentialsException();
                         } else {
@@ -52,10 +62,11 @@ public class WebkioskCgpaReport implements WebkioskContract<CgpaReportResult>{
                                     .getElementsByTag("table")
                                     .get(2).getElementsByTag("tbody")
                                     .get(0).children();
+                            /*get the table which contains the content here by analyzing the DOM*/
 
                             for (Element element : elements) {
                                 Elements subElements = element.children();
-
+                                /*traverse element by element to get the data*/
                                 CgpaReport cgpaReport = new CgpaReport();
                                 cgpaReport.setSemesterIndex(Integer.parseInt(StringUtility.cleanString(subElements.get(0).text())));
                                 cgpaReport.setGradePoints(Double.parseDouble(StringUtility.cleanString(subElements.get(1).text())));
@@ -67,10 +78,13 @@ public class WebkioskCgpaReport implements WebkioskContract<CgpaReportResult>{
                                 cgpaReport.setCgpa(Double.parseDouble(StringUtility.cleanString(subElements.get(7).text())));
 
                                 cgpaReportResult.getCgpaReports().add(cgpaReport);
+
+                                /*creating the result arraylist*/
                             }
 
                         }
 
+                        /*check if user has provided the callbak for the result of the api called using the library*/
                         mResultHandler.post(new Runnable() {
                             @Override
                             public void run() {
