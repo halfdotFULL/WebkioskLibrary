8747013055fa95d11149aa0e304843ce4cfcbd6a
NexFlare <NexFlare@users.noreply.github.com>
2017-10-03 08:28:57 +0530

    Login Screen Added

--- webkiosklibrary2/src/main/java/com/nexflare/webkiosklibrary/Utils/Constants.java
+++ webkiosklibrary2/src/main/java/com/nexflare/webkiosklibrary/Utils/Constants.java
@@ -7,7 +7,7 @@ package com.nexflare.webkiosklibrary.Utils;
 public class Constants {
 
     public static final String URL_QUERY_PARAM = "?x=&exam=";
-    public static final String BASE_URL_THAPAR="https://webkiosk.thapar.edu";
+    public static final String BASE_URL_THAPAR="https://webkiosk.thapar.edu/";
     public static final String BASE_URL_JIIT="https://webkiosk.jiit.ac.in/";
     public static final String BASE_URL_JIIT_LOGIN="https://webkiosk.jiit.ac.in/StudentFiles/StudentPage.jsp";
     public static final String BASE_URL_THAPAR_LOGIN="https://webkiosk.thapar.edu/StudentFiles/StudentPage.jsp";
