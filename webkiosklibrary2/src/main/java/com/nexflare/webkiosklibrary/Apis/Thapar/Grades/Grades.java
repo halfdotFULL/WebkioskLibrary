b3f90599faf33c2ae4473e8145b3bcfe75b566b2
aakarshak14 <akki.agg14@gmail.com>
2017-11-22 22:42:34 +0530

    Grade.java Added for Thapar

---
+++ webkiosklibrary2/src/main/java/com/nexflare/webkiosklibrary/Apis/Thapar/Grades/Grades.java
@@ -0,0 +1,44 @@
+package com.nexflare.webkiosklibrary.Apis.Thapar.Grades;
+
+/**
+ * Created by nexflare on 27/09/17.
+ */
+
+public class Grades {
+    private String subjectName;
+    private String subjectCode;
+    private String examCode;
+    private String examGrade;
+
+    public String getSubjectName() {
+        return subjectName;
+    }
+
+    public void setSubjectName(String subjectName) {
+        this.subjectName = subjectName;
+    }
+
+    public String getSubjectCode() {
+        return subjectCode;
+    }
+
+    public void setSubjectCode(String subjectCode) {
+        this.subjectCode = subjectCode;
+    }
+
+    public String getExamCode() {
+        return examCode;
+    }
+
+    public void setExamCode(String examCode) {
+        this.examCode = examCode;
+    }
+
+    public String getExamGrade() {
+        return examGrade;
+    }
+
+    public void setExamGrade(String examGrade) {
+        this.examGrade = examGrade;
+    }
+}
